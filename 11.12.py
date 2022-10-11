2db155d7e222c658086a319e06b7bb4de6ec5aaf
tothk3 <112930894+tothk3@users.noreply.github.com>
2022-10-11 09:39:17 +0200

    Add files via upload

---
+++ 11.12.py
@@ -0,0 +1,31 @@
+'''gondolt_szám = 4
+tipp = input('gondolzam wgy számra.    Tippeld meg!!!')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print('ügyes!')
+    print('Gratulálok')
+else:
+    print('Hosszan Gondolkodtál Rajta?:)')
+    print('Nem Érte Meg.;)')
+print('PÁPÁ')
+'''
+
+
+szam1 = input(int("agyá szamot"))
+szam2 = input(int("agyá szamot"))
+
+
+if szam1 == szam2:
+    print("megegyzik")
+
+if szam1 != szam2:
+    print("megegyezik")
+
+if szam1 > szam2:
+    print("szam1 > szam2")
+
+else:
+    print("az elsö szam > vagy = a szam 2 nél")
+
+
+print("a program lefutot")
