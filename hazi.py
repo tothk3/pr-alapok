da7db9df141a745baf0be6221ebb411a4c196fad
tothk3 <112930894+tothk3@users.noreply.github.com>
2022-10-13 11:56:12 +0200

    Add files via upload

--- hazi.py
+++ hazi.py
@@ -1,8 +1,22 @@
-elso_szam = int(input("Kérek egy számot 1-100ig:"))
-masodik_szam = int(input("Kérek egy másik számot 1-100ig:"))
+import math
 
-if elso_szam or masodik_szam > 100:
-    print("asas")
+szam = input("irja be egy szamot  ")
+szam1 = input("na meg egyet ")
+szam = int(szam)
+szam1 = int(szam1)
+a = szam - szam1
+b = abs(a)
+
+if szam == szam1:
+    print(f"Az első szam {szam} egyenlő a masodikal {szam1}")
+    
+
+elif szam < szam1: 
+    print("2 szam nem egyezik")
+    print("A külömbség", b , "a", szam, "kisseb mint ",szam1)
+  
 else:
-    print(elso_szam + masodik_szam)
-    print(elso_szam * masodik_szam)
+    print("2 szam nem egyezik")
+    print("A külömbség", b , "a", szam1, "kisseb mint ",szam)
+    
+quit()
