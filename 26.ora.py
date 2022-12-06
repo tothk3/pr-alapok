c30894987f85ec58013b53ecdbe79992a9679b80
tothk3 <112930894+tothk3@users.noreply.github.com>
2022-12-06 09:40:10 +0100

    Add files via upload

---
+++ 26.ora.py
@@ -0,0 +1,59 @@
+"""lista = ["asd", "dsa", "sda"]
+print(lista)"""
+
+"""lista = ["asd", "dsa", "sda", "a","r"]
+print(lista)
+
+lista = ["asd", "dsa", "sda", "a","r"]
+print(len(lista))"""
+
+"""lista = ["asd", "dsa", "sda", "a","r"]
+lista1 = [1, 2, 3, 4, 5]
+lista2 = [True ,False ,True ,False]"""
+
+"""lista = [False, 2, "sda", True, "r"]
+print(lista)"""
+
+"""lista = ["asd", "dsa", "sda"]
+print(type(lista))"""
+
+"""lista = list (("asd", "dsa", "sda"))
+print(lista)"""
+
+"""lista = [False, 2, "sda", True, "r"]
+print(lista[1])"""
+"""
+lista = [False, 2, "sda", True, "r"]
+print(lista[-1])"""
+
+"""lista = [False, 2, "sda-1", True, "r-2"]
+print(lista[2:5])"""
+
+"""lista = ["alam" ,"bannan" ,"narncs", "kiwi", "mango"]
+print(lista[1:4])"""
+
+"""lista = ["alam" ,"bannan" ,"narncs", "kiwi", "mango"]
+print(lista[-4:-1])"""
+
+"""lista = ["alam" ,"bannan" ,"narncs", "kiwi", "mango"]
+print(lista[-4:])"""
+
+"""lista = ["alam" ,"bannan" ,"narncs", "kiwi", "mango"]
+if "bannan" in lista:
+    print("asdasdasdads")"""
+
+"""lista = ["alam" ,"bannan" ,"narncs", "kiwi", "mango"]
+lista[1] = "kiwi"
+print(lista)"""
+
+"""lista = ["alam" ,"bannan" ,"narncs", "kiwi", "mango"]
+lista[1:2] = ["körte","szilva"]
+print(lista)"""
+
+"""lista = ["alam" ,"bannan" ,"narncs", "kiwi", "mango"]
+lista.insert(2, "körte")
+print(lista)"""
+
+lista = ["alam" ,"bannan" ,"narncs", "kiwi", "mango"]
+lista.append("körte")
+print(lista)
