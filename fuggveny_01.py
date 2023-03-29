7bb652aa9b53b20eedb1585ba9c517fa9aaa03ce
tothk3 <112930894+tothk3@users.noreply.github.com>
2023-03-29 08:19:42 +0200

    Add files via upload

---
+++ fuggveny_01.py
@@ -0,0 +1,8 @@
+# ord()		Python ord() function takes string argument of a single Unicode character and return its integer Unicode code point value.
+# chr()     The chr() method converts an integer to its unicode character and returns it.
+
+teszt_lista = [0,1,2,3,4,5,6,7,8,9]
+
+print(min(teszt_lista))
+print(max(teszt_lista))
+print(teszt_lista[2])
